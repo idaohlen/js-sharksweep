9ac9c99d0162167c68b631ee14bd21549e810f61
idaohlen <ida-alexandra.ohlen@chasacademy.se>
2024-10-26 13:00:23 +0200

    colors

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const grid = document.querySelector(".grid");
 const gridSizeLabel = document.querySelector(".options__grid-size");
 const playBtn = document.querySelector(".play-btn");
 
-let gridSize = 3;
+let gridSize = 5;
 let minesAmount = 3;
 let field = [];
 let mines = [];
@@ -52,30 +52,26 @@ function randomizeMines(count) {
   }
 }
 
+function getNeighborsOfCell(x, y) {
+  return {
+    north:     field?.[x - 1]?.[y],
+    northEast: field?.[x - 1]?.[y + 1],
+    east:      field?.[x]?.[y + 1],
+    southEast: field?.[x + 1]?.[y + 1],
+    south:     field?.[x + 1]?.[y],
+    southWest: field?.[x + 1]?.[y - 1],
+    west:      field?.[x]?.[y - 1],
+    northWest: field?.[x - 1]?.[y - 1]
+  }
+}
+
 function placeMines() {
   mines.forEach(mine => {
-    // Debug: Display mines
-    // field[mine.x][mine.y].element.innerHTML = "🔴";
-
-    const neighbors = {
-      north:     field?.[mine.x - 1]?.[mine.y],
-      northEast: field?.[mine.x - 1]?.[mine.y + 1],
-      east:      field?.[mine.x]?.[mine.y + 1],
-      southEast: field?.[mine.x + 1]?.[mine.y + 1],
-      south:     field?.[mine.x + 1]?.[mine.y],
-      southWest: field?.[mine.x + 1]?.[mine.y - 1],
-      west:      field?.[mine.x]?.[mine.y - 1],
-      northWest: field?.[mine.x - 1]?.[mine.y - 1]
-    }
+    const neighbors = getNeighborsOfCell(mine.x, mine.y);
 
     for (const neighbor of Object.values(neighbors)) {
       if (neighbor) {
         neighbor.adjacentMines++;
-
-        // Debug: Display adjacent mines count
-        // if (!neighbor.mine) {
-        //   neighbor.element.innerHTML = neighbor.adjacentMines;
-        // }
       }
     }
   });
@@ -85,6 +81,8 @@ function revealField() {
   for (let x = 0; x < gridSize; x++) {
     for (let y = 0; y < gridSize; y++) {
       const cell = field[x][y];
+      styleCell(cell);
+
       if (cell.mine) {
         cell.element.textContent = "🔴";
       } else {
@@ -94,17 +92,63 @@ function revealField() {
   }
 }
 
+function fillNeighbours(neighbors) {
+  for (const neighbor of Object.values(neighbors)) {
+    if (neighbor && !neighbor.mine && !neighbor.cleared) {
+      neighbor.cleared = true;
+      clearedCells++;
+      styleCell(neighbor);
+      neighbor.element.textContent = neighbor.adjacentMines;
+    }
+  }
+}
+
+function styleCell(cell) {
+  if (cell.mine) {
+    cell.element.classList.add("mine");
+    cell.element.textContent = "🔴";
+  } else {
+    cell.element.textContent = cell.adjacentMines;
+
+    switch (cell.adjacentMines) {
+      case 0:
+        cell.element.classList.add("empty");
+      case 1:
+        cell.element.classList.add("close");
+        break;
+      case 2:
+        cell.element.classList.add("warning");
+        break;
+      case 3:
+        cell.element.classList.add("danger");
+        break;
+      default: break;
+    }
+  }
+}
+
 function pickCell(x, y) {
   if (playing) {
     const cell = field[x][y];
 
+    // Game over is the cell is a mine
     if (cell.mine) {
-      cell.element.textContent = "🔴";
       gameOver();
-    } else {
-      clearedCells++;
-      cell.element.textContent = cell.adjacentMines;
     }
+    // Cell is not a mine
+    else {
+      if(!cell.cleared) {
+        clearedCells++;
+
+        // Check if cell does not have adjacent mines
+        if (cell.adjacentMines === 0) {
+          console.log("No adjacent mines");
+          const neighbors = getNeighborsOfCell(x, y);
+          fillNeighbours(neighbors);
+        }
+      }
+    }
+    styleCell(cell);
 
     if (clearedCells == (gridSize * gridSize) - minesAmount) {
       win();
@@ -115,8 +159,8 @@ function pickCell(x, y) {
 function play() {
   mines = [];
   clearedCells = 0;
-
   playing = true;
+
   renderGrid();
   randomizeMines(minesAmount);
   placeMines();
